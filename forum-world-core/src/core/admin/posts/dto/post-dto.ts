818612423a6cc48e9d4862d692594480be0805da
777LK777 <konoplin.7777@gmail.com>
2023-05-09 15:55:53 +0400

    Backend_PostsReworks

--- forum-world-core/src/core/admin/posts/dto/post-dto.ts
+++ forum-world-core/src/core/admin/posts/dto/post-dto.ts
@@ -1,9 +1,12 @@
+import { ThemeNameDto as Theme } from "../../themes/dto/theme-dto";
+import { CountryNameDto as Country } from "../../countries/dto/country-dto";
+
 export class PostDto {
     constructor(
         readonly id: number,
         readonly name: string,
-        readonly countryId: number,
-        readonly themeId?: number,
+        readonly country: Country,
+        readonly theme?: Theme,
         readonly contentId?: number
     ) { }
 }
