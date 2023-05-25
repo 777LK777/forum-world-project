5879a4fccb127fadd40f0227baea774135f0eec4
777LK777 <konoplin.7777@gmail.com>
2023-05-25 17:43:21 +0400

    Debugging

--- forum-world-gateway/src/core-api/public/contents/contents.module.ts
+++ forum-world-gateway/src/core-api/public/contents/contents.module.ts
@@ -3,7 +3,7 @@ import { ContentsService } from './contents.service';
 import { CoreApiModule } from 'src/core-api/_shared/core-api-module/core-api.module';
 
 @Module({
-  imports: [CoreApiModule.forFeature('public/contents')],
+  imports: [CoreApiModule.forFeature('/public/contents')],
   providers: [ContentsService],
   exports: [ContentsService]
 })
